f3e4966b0cae5c65422e5e59822aad4a6449710d
Shimmita <douglasshimita3@gmail.com>
2023-12-09 12:01:10 +0300

    finished quiz one and it can output the deference between two years using OOP aproach

---
+++ date_deference.php
@@ -0,0 +1,23 @@
+<?php
+class DateDiff {
+    
+    //declaring the start and end dates public varibles
+    public $startDate, $endDate;
+    public function __construct($startDate, $endDate) {
+        $this->startDate = new DateTime($startDate);
+        $this->endDate = new DateTime($endDate);
+    }
+
+    //function to calculate the date deference
+    public function calculateDiff() {
+        $interval = $this->startDate->diff($this->endDate);
+        return "Difference: " . $interval->y . " years, " . $interval->m . " months, " . $interval->d . " days";
+    }
+}
+
+
+//pass two dates within the constructor and then their deference will be calculate
+$dateDiff = new DateDiff("1981-11-03", "2013-09-04");
+
+//output the result
+echo $dateDiff->calculateDiff();
