2c41674469dee1e2ea3985b719f52f78ca30f984
Shimmita <douglasshimita3@gmail.com>
2023-12-09 12:49:48 +0300

    finished quiz two  and it can output the majority element in a given array

--- majority_element_in_array.php
+++ majority_element_in_array.php
@@ -5,21 +5,21 @@ function majority_element_in_array($arr)
     //current index init
         $current_index = 0;
         //counts the number of occurencess
-        $occurence_counter = 1;
+        $element_occurence_counter = 1;
         
         for($i=1; $i<sizeof($arr); $i++)
         {
             if ($arr[$current_index] == $arr[$i])
             {
-                $occurence_counter += 1;
+                $element_occurence_counter += 1;
             }
             else
              {
-                $occurence_counter -= 1;
-                if ($occurence_counter == 0)
+                $element_occurence_counter -= 1;
+                if ($element_occurence_counter == 0)
                 {
                     $current_index = $i;
-                    $occurence_counter = 1;
+                    $element_occurence_counter = 1;
                 }
           }   
         }      
